52e3c7cce423e53b7f12a083edb64b64a3013ea4
sergio <sergiohidalgocaceres@gmail.com>
2018-11-08 19:15:25 -0500

    * Modificación de la aplicacion para conexión a mongodb

--- paymentGatewayBackend/server.ts
+++ paymentGatewayBackend/server.ts
@@ -3,8 +3,6 @@ import _DOTENV = require("dotenv");
 import _mongoose = require("mongoose");
 import _http = require("http");
 
-import MainApp from "./main";
-
 //Configuring DOTENV
 _DOTENV.config({ path: "./variables.env" })
 
@@ -15,5 +13,7 @@ _mongoose.connection.on("connected", () => console.log("Connecting to MongoDB"))
 _mongoose.connection.on("error", error => console.log(error));
 
 //Configuring Http Server
+import MainApp from "./main";
+
 const servidorHttp = _http.createServer(MainApp);
 servidorHttp.listen(MainApp.get("PORT"), () => console.log(`Server running on port ${MainApp.get("PORT")}`));
