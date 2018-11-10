0a0f1d85f11ce4e419043dbfe78bc007ec66c226
brayan_reyes <bcrg.1993@gmail.com>
2018-11-10 13:58:22 -0500

    * Modificacion de server.ts

--- paymentGatewayBackend/server.ts
+++ paymentGatewayBackend/server.ts
@@ -3,10 +3,8 @@ import _DOTENV = require("dotenv");
 import _mongoose = require("mongoose");
 import _http = require("http");
 
-import MainApp from "./main";
-
 //Configuring DOTENV
-_DOTENV.config({ path: "./variables.env" })
+_DOTENV.config({ path: "./variables.env" });
 
 //Configuring mongoose
 _mongoose.Promise = global.Promise;
@@ -15,5 +13,7 @@ _mongoose.connection.on("connected", () => console.log("Connecting to MongoDB"))
 _mongoose.connection.on("error", error => console.log(error));
 
 //Configuring Http Server
+import MainApp from "./main";
+
 const servidorHttp = _http.createServer(MainApp);
 servidorHttp.listen(MainApp.get("PORT"), () => console.log(`Server running on port ${MainApp.get("PORT")}`));
